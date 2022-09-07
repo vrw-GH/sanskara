db5c2bd74514914c561db7de77e450a80586a09d
Victor Wright <victor.wright@outlook.de>
2022-09-07 20:01:49 +0200

    usingreact-ticker

--- src/components/Marquee.js
+++ src/components/Marquee.js
@@ -1,4 +1,5 @@
-import Mq1 from 'react-easy-marquee'; //! React 17 ???
+// import Mq1 from 'react-easy-marquee'; //! React 17 ???
+import Ticker from 'react-ticker';
 
 export const Marquee = ({ APPDATA }) => {
   const marqueeText = (
@@ -21,9 +22,12 @@ export const Marquee = ({ APPDATA }) => {
 
   return (
     <>
-      <Mq1 duration="20000" height="1rem" background="red" pauseOnHover={true}>
+      {/* <Mq1 duration="20000" height="1rem" background="red" pauseOnHover={true}>
         {marqueeText}
-      </Mq1>
+      </Mq1> */}
+      <Ticker speed={10} direction="toRight" mode="smooth">
+        {() => <>{marqueeText}</>}
+      </Ticker>
     </>
   );
 };
